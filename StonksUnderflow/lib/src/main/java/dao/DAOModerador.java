52b99146d096b9a3c57a2a76b0b76354a3f7bf39
CayoNikolas <cayo.santos@arapiraca.ufal.br>
2023-05-23 08:33:48 -0300

    DAOModerador e IDAOModerador

--- StonksUnderflow/lib/src/main/java/dao/DAOModerador.java
+++ StonksUnderflow/lib/src/main/java/dao/DAOModerador.java
@@ -8,53 +8,53 @@ import org.hibernate.Session;
 import org.hibernate.Transaction;
 
 import hibernate.HibernateConfiguration;
-import model.Usuario;
+import model.Moderador;
 
-public class DAOUsuario implements IDAOUsuario{
+public class DAOModerador implements IDAOModerador{
 	private Session session;
 	
-	public DAOUsuario() {
+	public DAOModerador() {
 		HibernateConfiguration config = new HibernateConfiguration();
 		this.session = config.getSession();
 	}
 	
 	@Override
-	public Usuario getUsuario(int id) {
-		 Usuario output = this.session.get(Usuario.class, id);
+	public Moderador getModerador(int id) {
+		 Moderador output = this.session.get(Moderador.class, id);
 		 return output;
 	}
 	
 	@Override
 	@SuppressWarnings("unchecked")
-	public List<Usuario> getUsuarios(){
-		List<Usuario> output = null;
-		String hql = "FROM Usuario u";
+	public List<Moderador> getModeradores(){
+		List<Moderador> output = null;
+		String hql = "FROM Moderador m";
 		Query query = this.session.createQuery(hql);
 		output = query.getResultList();
 		return output;
 	}
 	
 	@Override
-	public void adicionarOuAlterarUsuario(Usuario u) {
+	public void adicionarOuAlterarModerador(Moderador m) {
 		Transaction t = this.session.beginTransaction(); //início da transação
-		this.session.saveOrUpdate(u);
+		this.session.saveOrUpdate(m);
 		t.commit(); //finalizando transação
 	}
 	
 	@Override
 	@SuppressWarnings("unchecked")
-	public List<Usuario> getUsuarios(String filtroNome){
-		List<Usuario> output = null;
-		String hql = "FROM Usuario u WHERE u.nome LIKE '%"+filtroNome+"%'";
+	public List<Moderador> getModeradores(String filtroNome){
+		List<Moderador> output = null;
+		String hql = "FROM Moderador m WHERE m.nome LIKE '%"+filtroNome+"%'";
 		Query query = this.session.createQuery(hql);
 		output = query.getResultList();
 		return output;
 	}
 	
 	@Override
-	public void apagarUsuario(Usuario u) {
+	public void apagarModerador(Moderador m) {
 		Transaction t = this.session.beginTransaction();
-		this.session.delete(u);
+		this.session.delete(m);
 		t.commit();
 	}
 	
